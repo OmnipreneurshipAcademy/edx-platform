efe96d69cf87d57ae8e89bb639abf2502d21a56d
javeriaejaz-arbisoft <javeria.ejaz@arbisoft.com>
2021-04-05 16:17:32 +0500

    LP-2600 Update webinars app name

---
+++ openedx/adg/lms/webinars/apps.py
@@ -0,0 +1,12 @@
+"""
+All configurations for webinars app
+"""
+from django.apps import AppConfig
+
+
+class WebinarsConfig(AppConfig):
+    """
+    Webinar app configuration.
+    """
+
+    name = 'openedx.adg.lms.webinars'

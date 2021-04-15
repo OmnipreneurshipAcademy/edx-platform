9dfb9315915a7ac5fef189a029ddb818d2cad797
javeriaejaz-arbisoft <javeria.ejaz@arbisoft.com>
2021-04-15 14:17:29 +0500

    LP-2816 Provide interface for admins to add emails for the webinar invitations

--- openedx/adg/lms/webinars/tests/test_admin.py
+++ openedx/adg/lms/webinars/tests/test_admin.py
@@ -3,12 +3,14 @@ All the tests related to admin in webinars app
 """
 import pytest
 from django.contrib.admin.sites import AdminSite
-from mock import Mock
+from mock import Mock, call
 
+from common.djangoapps.student.tests.factories import UserFactory
+from openedx.adg.common.lib.mandrill_client.client import MandrillClient
 from openedx.adg.lms.webinars.admin import ActiveWebinarStatusFilter, CancelledWebinarAdmin, WebinarAdmin
 from openedx.adg.lms.webinars.models import CancelledWebinar, Webinar
 
-from .factories import WebinarFactory
+from .factories import WebinarFactory, WebinarRegistrationFactory
 
 
 @pytest.fixture(name='cancelled_webinar_admin')
@@ -115,3 +117,55 @@ def test_webinar_admin_custom_status_list_filter(
 
     delivered_filter = ActiveWebinarStatusFilter(None, {'status': Webinar.DELIVERED}, Webinar, WebinarAdmin)
     assert delivered_filter.queryset(None, webinar_admin_queryset).count() == expected_delivered_count
+
+
+@pytest.mark.django_db
+def test_send_update_emails_to_registrants_not_in_webinar_create(webinar_admin_instance, request):
+    """
+    Test that 'send_update_emails_to_registrants' is not in fields when creating webinar.
+    """
+    fields = WebinarAdmin.get_fields(webinar_admin_instance, request, None)
+    assert 'send_update_emails_to_registrants' not in fields
+
+
+@pytest.mark.django_db
+def test_send_update_emails_to_registrants_in_webinar_update(webinar_admin_instance, request):
+    """
+    Test that 'send_update_emails_to_registrants' is in fields when modifying webinar.
+    """
+    fields = WebinarAdmin.get_fields(webinar_admin_instance, request, WebinarFactory())
+    assert 'send_update_emails_to_registrants' in fields
+
+
+@pytest.mark.django_db
+def test_save_related_send_update_emails(request, webinar_admin_instance, webinar, mocker):
+    """
+     Test that upon updating webinar, emails for updated invites are send.
+    """
+    mock_send_webinar_emails = mocker.patch('openedx.adg.lms.webinars.admin.send_webinar_emails')
+    mock_form_class = mocker.patch('openedx.adg.lms.webinars.forms.WebinarForm')
+    request.method = 'POST'
+    mock_form_class.instance = webinar
+    user = UserFactory()
+    WebinarRegistrationFactory(user=user, webinar=webinar)
+    WebinarAdmin.save_related(webinar_admin_instance, request, mock_form_class, [], True)
+    mock_send_webinar_emails.assert_has_calls([
+        call(MandrillClient.WEBINAR_UPDATED, webinar.title, webinar.description, webinar.start_time, [user.email]),
+        call(MandrillClient.WEBINAR_CREATED, webinar.title, webinar.description, webinar.start_time, [])
+    ])
+
+
+@pytest.mark.django_db
+def test_save_related_send_invite_emails(request, webinar_admin_instance, webinar, mocker):
+    """
+     Test that upon creating webinar emails for only invites are send.
+    """
+
+    mock_send_webinar_emails = mocker.patch('openedx.adg.lms.webinars.admin.send_webinar_emails')
+    mock_form_class = mocker.patch('openedx.adg.lms.webinars.forms.WebinarForm')
+    request.method = 'POST'
+    mock_form_class.instance = webinar
+    WebinarAdmin.save_related(webinar_admin_instance, request, mock_form_class, [], False)
+    mock_send_webinar_emails.assert_called_once_with(
+        MandrillClient.WEBINAR_CREATED, webinar.title, webinar.description, webinar.start_time, []
+    )
--- openedx/adg/lms/webinars/tests/test_helpers.py
+++ openedx/adg/lms/webinars/tests/test_helpers.py
@@ -9,9 +9,12 @@ import pytest
 from common.djangoapps.student.tests.factories import UserFactory
 from openedx.adg.common.lib.mandrill_client.client import MandrillClient
 from openedx.adg.lms.webinars.helpers import (
+    remove_emails_duplicate_in_other_list,
     send_cancellation_emails_for_given_webinars,
     send_webinar_emails,
-    send_webinar_registration_email
+    send_webinar_registration_email,
+    validate_email_list,
+    webinar_emails_for_panelist_co_hosts_and_presenter
 )
 
 from .factories import WebinarFactory, WebinarRegistrationFactory
@@ -121,3 +124,68 @@ def test_send_webinar_registration_email(mocker):
         'webinar_link': webinar.meeting_link,
         'webinar_start_time': 'Jan 01, 2020 01:10 PM GMT',
     })
+
+
+@pytest.mark.parametrize(
+    'emails , expected_error', [
+            ('test1@exmaple.com,test2@example.com', False),
+            ('test1@exmaple.com,test2.example.com', True)
+        ]
+    )
+def test_validate_email_list(emails, expected_error):
+    """
+    Test if 'validate_email_list' returns error when emails are invalid
+    """
+    assert validate_email_list(emails)[0] == expected_error
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    'panelists, co_hosts, presenter, expected_emails', [
+            (['pl1@eg.com', 'pl2@eg.com'], [], 'presenter@eg.com', ['pl1@eg.com', 'pl2@eg.com', 'presenter@eg.com']),
+            ([], ['ch1@eg.com', 'ch2@eg.com'], 'presenter@eg.com', ['ch1@eg.com', 'ch2@eg.com', 'presenter@eg.com']),
+            ([], [], 'presenter@eg.com', ['presenter@eg.com']),
+            (
+                ['pl1@eg.com', 'pl2@eg.com'],
+                ['ch1@eg.com', 'ch2@eg.com'],
+                'presenter@eg.com',
+                ['pl1@eg.com', 'pl2@eg.com', 'ch1@eg.com', 'ch2@eg.com', 'presenter@eg.com']
+            ),
+
+        ]
+    )
+def test_webinar_emails_for_panelist_co_hosts_and_presenter(webinar, panelists, co_hosts, presenter, expected_emails):
+    """
+    Test if 'webinar_emails_for_panelist_co_hosts_and_presenter' returns the list of webinar related emails
+    """
+    panelist_emails = factory.Iterator(panelists)
+    panelist_users = UserFactory.create_batch(len(panelists), email=panelist_emails)
+    webinar.panelists.add(*panelist_users)
+
+    co_host_emails = factory.Iterator(co_hosts)
+    co_host_users = UserFactory.create_batch(len(co_hosts), email=co_host_emails)
+    webinar.co_hosts.add(*co_host_users)
+
+    webinar.presenter = UserFactory(email=presenter)
+    assert sorted(webinar_emails_for_panelist_co_hosts_and_presenter(webinar)) == sorted(expected_emails)
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    'emails, reference_emails, expected_emails', [
+            ([], ['t1@eg.com', 't2@eg.com'], []),
+            (['t1@eg.com', 't2@eg.com'], [], ['t1@eg.com', 't2@eg.com']),
+            ([], [], []),
+            (
+                ['t1@eg.com', 't2@eg.com', 't3@eg.com'],
+                ['t1@eg.com', 't2@eg.com', 't4@eg.com'],
+                ['t3@eg.com']
+            ),
+
+        ]
+    )
+def test_remove_emails_duplicate_in_other_list(emails, reference_emails, expected_emails):
+    """
+    Test that only the list of emails that are not present in reference list of emails are return
+    """
+    assert sorted(remove_emails_duplicate_in_other_list(emails, reference_emails)) == sorted(expected_emails)

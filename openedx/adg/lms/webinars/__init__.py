cee7a44010856f43de120c57ccd0cf1ea9e2b922
harrisirfan <harris.irfan@arbisoft.com>
2021-04-30 12:51:40 +0500

    LP-2676 Remove extra details

--- openedx/adg/lms/webinars/__init__.py
+++ openedx/adg/lms/webinars/__init__.py
@@ -12,18 +12,6 @@ This feature enables ADG admins to:
  - view all events i.e. upcoming, cancelled and delivered
  - view list of registered users against events
 
-While creating a new event, admins can:
- - add event details like title, schedule, description, banner image, external meeting link for webinar
- - add presenters, co-hosts and panelists
- - choose event language out of English and Arabic
- - send email invites to all Omni Academy users
- - send email invites to outsiders by providing their email addresses
-
-While updating an existing event, admins can:
- - update event details, language, presenters, co-hosts and panelists
- - send new invites
- - send update email to all registered users
-
 Learner Experience
 ------------------
 
@@ -33,9 +21,11 @@ It also enables ADG learners to:
  - register for an upcoming event and attend a webinar via an external link
  - cancel registration
 
-When an event is cancelled by an admin, all users registered in the event receive a cancellation email.
 
-All registered users of an event also receive automated reminder emails.
+Upon new event creation, invitations are sent via email. All registered users
+ - can receive event update email
+ - receive event cancellation email
+ - receive automated reminder emails
 """
 
 default_app_config = 'openedx.adg.lms.webinars.apps.WebinarsConfig'
